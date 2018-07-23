9af673c89b0a3c43777951f788249e1567ca7560
Priscilla Okawa <prisoka@gmail.com>
2018-07-22 17:51:59 -0700

    setup CSS

--- react-inbox/src/index.js
+++ react-inbox/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'font-awesome/css/font-awesome.css';
-import './index.css';
 
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
