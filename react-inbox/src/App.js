b65dd93d5d6fa7e0a4640b9494df24d954b3c18f
Priscilla Okawa <prisoka@gmail.com>
2018-07-22 18:09:13 -0700

    working on Component Toolbar

--- react-inbox/src/App.js
+++ react-inbox/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Toolbar from './Components/Toolbar'
+import MessageList from './Components/MessageList'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Toolbar />
+        <MessageList />
       </div>
     );
   }
