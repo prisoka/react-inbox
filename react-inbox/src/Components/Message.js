0a8b304854c4b7bacc9702d7ec7808550404befb
Priscilla Okawa <prisoka@gmail.com>
2018-07-22 18:04:46 -0700

    working on Component Message

--- react-inbox/src/Components/Message.js
+++ react-inbox/src/Components/Message.js
@@ -3,8 +3,24 @@ import React, { Component } from 'react';
 const Message = (props) => {
   render() {
     return (
-      <div className="App">
-
+      <div className="row message read">
+        <div className="col-xs-1">
+          <div className="row">
+            <div className="col-xs-2">
+              <input type="checkbox" />
+            </div>
+            <div className="col-xs-2">
+              <i className="star fa fa-star"></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-11">
+          <span className="label label-warning">dev</span>
+          <span className="label label-warning">gschool</span>
+          <a href="#">
+            Here is some message text that has a bunch of stuff
+          </a>
+        </div>
       </div>
     );
   }
--- react-inbox/src/Components/MessageList.js
+++ react-inbox/src/Components/MessageList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Message from './Message'
 
 class MessageList extends Component {
   render() {
     return (
       <div className="App">
-
+        <Message />
       </div>
     );
   }
